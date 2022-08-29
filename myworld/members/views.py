7fa263692591229da2f8fb13bb9f2be21c4c368a
dastenturlin <dasten.turlin@nu.edu.kz>
2022-08-30 02:34:59 +0600

    Added templates

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -6,6 +6,8 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
