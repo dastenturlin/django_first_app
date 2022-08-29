248945e25f07e01f3fb0ebe71b4d99856d65ba48
dastenturlin <dasten.turlin@nu.edu.kz>
2022-08-30 02:20:42 +0600

    First routs

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
 
 # Create your views here.
 
-# Django views are Python functions that takes http requests and returns http response, like HTML documents.
+# Django views are Python functions that takes http requests and
+# returns http response, like HTML documents.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world!")
