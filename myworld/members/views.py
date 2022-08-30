6dceda15d4c9d29e4e0e7f95be6ed76d3ef542ef
dastenturlin <dasten.turlin@nu.edu.kz>
2022-08-30 18:20:51 +0600

    Added members to the model

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -4,10 +4,14 @@
 # Django views are Python functions that takes http requests and
 # returns http response, like HTML documents.
 
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def index(request):
+    mymembers = Members.objects.all().values()
+    output = ""
+    for x in mymembers:
+        output = output + " " + x["firstname"]
     template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    return HttpResponse(output)
